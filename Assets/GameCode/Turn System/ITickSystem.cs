e048741a97221946a8dbe602f7fef7edd6d55488
NickSamizdat <NickSchade@users.noreply.github.com>
2019-10-29 00:04:52 -0700

    tick system and buildqueue

---
+++ Assets/GameCode/Turn System/ITickSystem.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public enum eTickSystem {TurnBased, SemiRealTime};
+
+public interface ITickSystem
+{
+    TickInfo GetTick(List<GraphicsData> graphicsData);
+}
+
